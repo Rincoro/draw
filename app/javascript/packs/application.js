8fd40bc84788c114f26a0c71f9d177fab0c5cae8
木下　夏鈴 <kisitakarin@kinenatsusuzunoMacBook-Pro.local>
2024-09-12 11:13:16 +0900

    final

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,7 @@
+import testDraw from '../components/testReact';
+
+export default testDraw;
+
 /* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
